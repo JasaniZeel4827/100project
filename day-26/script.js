3f1791ffe3eff888fe9fbdd672d72e94390a0f68
Zeel <deltapixel8a@gmail.com>
2025-05-05 19:27:24 +0530

    100project

--- day-26/script.js
+++ day-26/script.js
@@ -11,4 +11,20 @@ slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
 
 const changeSlide = (direction) => {
   const sliderHeight = sliderContainer.clientHeight;
-  
+  if (direction === "up") {
+    activeSlideIndex++;
+    if (activeSlideIndex > slidesLength - 1) activeSlideIndex = 0;
+  } else if (direction === "down") {
+    activeSlideIndex--;
+    if (activeSlideIndex < 0) activeSlideIndex = slidesLength - 1;
+  }
+  slideRight.style.transform = `translateY(-${
+    activeSlideIndex * sliderHeight
+  }px)`;
+  slideLeft.style.transform = `translateY(${
+    activeSlideIndex * sliderHeight
+  }px)`;
+};
+
+upButton.addEventListener("click", () => changeSlide("up"));
+downButton.addEventListener("click", () => changeSlide("down"));
