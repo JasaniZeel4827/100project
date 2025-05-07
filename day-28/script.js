bc5abc08df0ccedd01a534cdb3295b8976c87fef
Zeel <deltapixel8a@gmail.com>
2025-05-07 16:12:45 +0530

    100project

--- day-28/script.js
+++ day-28/script.js
@@ -3,33 +3,7 @@ const form = document.getElementById("form");
 const main = document.getElementById("main");
 const search = document.getElementById("search");
 
-const createUserCard = (user) => {
-  const cardHTML = `
-    <div class="card">
-        <div>
-          <img
-            src="${user.avatar_url}"
-            alt="${user.name}"
-            class="avatar"
-          />
-        </div>
-        <div class="user-info">
-          <h2>${user.name}</h2>
-          <p>
-          ${user.bio}
-          </p>
-          <ul>
-            <li>${user.followers}<strong>Followers</strong></li>
-            <li>${user.following}<strong>Following</strong></li>
-            <li>${user.public_repos}<strong>Repos</strong></li>
-          </ul>
-          <div id="repos">
-          </div>
-        </div>
-      </div>
-    `;
-  main.innerHTML = cardHTML;
-};
+
 
 const createErrorCard = (message) => {
   const cardHTML = `
