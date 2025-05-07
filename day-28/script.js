2a0c908667206db8077c4c722f8f6370086f1737
Zeel <deltapixel8a@gmail.com>
2025-05-07 16:13:01 +0530

    100project

--- day-28/script.js
+++ day-28/script.js
@@ -3,7 +3,33 @@ const form = document.getElementById("form");
 const main = document.getElementById("main");
 const search = document.getElementById("search");
 
-
+const createUserCard = (user) => {
+  const cardHTML = `
+    <div class="card">
+        <div>
+          <img
+            src="${user.avatar_url}"
+            alt="${user.name}"
+            class="avatar"
+          />
+        </div>
+        <div class="user-info">
+          <h2>${user.name}</h2>
+          <p>
+          ${user.bio}
+          </p>
+          <ul>
+            <li>${user.followers}<strong>Followers</strong></li>
+            <li>${user.following}<strong>Following</strong></li>
+            <li>${user.public_repos}<strong>Repos</strong></li>
+          </ul>
+          <div id="repos">
+          </div>
+        </div>
+      </div>
+    `;
+  main.innerHTML = cardHTML;
+};
 
 const createErrorCard = (message) => {
   const cardHTML = `
@@ -24,16 +50,7 @@ const addReposToCard = (repos) => {
   });
 };
 
-const getUser = async (username) => {
-  try {
-    const { data } = await axios(APIURL + username);
-    createUserCard(data);
-    getRepos(username);
-  } catch (error) {
-    if (error.response.status == 404)
-      createErrorCard("No profile with this username");
-  }
-};
+
 
 const getRepos = async (username) => {
   try {
