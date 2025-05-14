b6e392fdf8fab9b4fd57a6c49ebbcc4a272d9fcc
Zeel <deltapixel8a@gmail.com>
2025-05-14 09:03:59 +0530

    100project

--- UPPERCASE_TO_LOWERCASE/script.js
+++ UPPERCASE_TO_LOWERCASE/script.js
@@ -2,6 +2,7 @@ function convertToLower() {
     const inputText = document.getElementById('textInput').value;
     const lowerText = inputText.toLowerCase();
     document.getElementById('output').textContent = lowerText;
+    return lowerText; // Add return statement
 }
 
 convertToLower();
